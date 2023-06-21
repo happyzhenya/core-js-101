3a6e2dfcc0a5a02838255adb5d753cd1d648293c
Sushkevich Zhenya <happy1@list.ru>
2023-06-21 21:43:32 +0300

    fix:fix the mistake

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -23,7 +23,7 @@ function Rectangle(width, height) {
   this.width = width;
   this.height = height;
 }
-Rectangle.prototype.getArea = function () {
+Rectangle.prototype.getArea = function x() {
   return this.width * this.height;
   // throw new Error('Not implemented');
 };
